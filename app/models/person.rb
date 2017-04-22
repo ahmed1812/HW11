e418988331659189b7e8847749e24b8562a43b88
ahmed ahmed <algandel19@gmail.com>
2017-04-22 01:27:26 +0000

    project

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ActiveRecord::Base
+     validates_presence_of :first_name ,:last_name , :email 
+     validates_length_of :first_name,:last_name ,  :minimum  => 3
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,4 @@
 class Vehicle < ActiveRecord::Base
+    validates_presence_of :vin, :year, :make , :model, :engine_type , :license_plate
+    validates_length_of :year ,  :minimum  => 4
 end
